1608db8f71080315fc6c60c4a004596b22bbe5b1
k0nghaa <k0nghaalee@gmail.com>
2025-11-18 09:57:41 +0900

    chore: 팀 코드 스타일을 위한 ESLint + Prettier 구성

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,32 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import importPlugin from "eslint-plugin-import";
+import prettier from "eslint-plugin-prettier";
 
-const eslintConfig = defineConfig([
+export default defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  {
+    plugins: {
+      prettier,
+      import: importPlugin,
+    },
+    extends: ["plugin:prettier/recommended"], // Prettier 규칙 우선
+    rules: {
+      "prettier/prettier": "error", // Prettier 위반 시 ESLint 오류
+      "import/order": [
+        "error",
+        {
+          groups: ["builtin", "external", "internal", ["parent", "sibling", "index"]],
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  },
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
-
-export default eslintConfig;
