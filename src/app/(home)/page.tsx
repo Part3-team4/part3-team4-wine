726ae4be98bf8c6fc3fad87e2550747223728aa3
9tnqls6 <9tnqls6@gmail.com>
2025-11-27 00:13:24 +0900

    feat:랜딩페이지 파일 생성 및 구조 잡기

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,20 @@
+import Style from '@/app/(home)/page.module.scss';
+import Button from '@/components/common/Button/Button';
+
 export default function Home() {
-  return <>홈</>;
+  return (
+    <>
+      <main className={Style.mainContent}>
+        <div className={Style.mainBanner}>mainBanner</div>
+        <div className={Style.mainContFrame}>
+          <div className={Style.mainCont}>mainCont</div>
+          <div className={Style.mainCont}>mainCont</div>
+          <div className={Style.mainCont}>mainCont</div>
+        </div>
+        <Button variant="filled" size="medium" rounded className={Style.mainBtn}>
+          와인 보러가기
+        </Button>
+      </main>
+    </>
+  );
 }
