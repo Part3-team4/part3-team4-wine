b4c69fba04ed5025fe07defcc27c9ee4c5fce42b
Dang <wheodnjs2@naver.com>
2025-11-26 22:36:31 +0900

    fix: 홈 레이아웃 수정

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -3,8 +3,10 @@ import Header from '@/components/common/Header/Header';
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="homeWrap">
-      <Header />
-      {children}
+      <div className="inner">
+        <Header />
+        {children}
+      </div>
     </div>
   );
 }
