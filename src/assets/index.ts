3cc84f8879e920f9ad493d963cf937eea17cbc98
9tnqls6 <9tnqls6@gmail.com>
2025-11-28 00:35:13 +0900

    refactor:이미지index 기존과 분리

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -21,17 +21,6 @@ import Warning from '@/assets/icons/ico_warning.png';
 import ProfileDefault from '@/assets/icons/ico_profile.png';
 import ProfileHover from '@/assets/icons/ico_profile_hover.png';
 
-//랜딩페이지
-import landingMainBannerLg from '@/assets/images/landing/lg_01.png';
-import landingMainBannerMd from '@/assets/images/landing/md_01.png';
-import landingMainBannerSm from '@/assets/images/landing/sm_01.png';
-import landingConImg01Lg from '@/assets/images/landing/lg_02.png';
-import landingConImg01Sm from '@/assets/images/landing/sm_02.png';
-import landingConImg02Lg from '@/assets/images/landing/lg_03.png';
-import landingConImg02Sm from '@/assets/images/landing/sm_03.png';
-import landingConImg03Lg from '@/assets/images/landing/lg_04.png';
-import landingConImg03Sm from '@/assets/images/landing/sm_04.png';
-
 export {
   Close,
   DropdownArrow,
@@ -54,13 +43,4 @@ export {
   Warning,
   ProfileDefault,
   ProfileHover,
-  landingMainBannerLg,
-  landingMainBannerMd,
-  landingMainBannerSm,
-  landingConImg01Lg,
-  landingConImg01Sm,
-  landingConImg02Lg,
-  landingConImg02Sm,
-  landingConImg03Lg,
-  landingConImg03Sm,
 };
