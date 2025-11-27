7678b767c0d795a78740b13953e4c412e5bf63da
9tnqls6 <9tnqls6@gmail.com>
2025-11-28 00:38:51 +0900

    refactor:이미지 import 방식 변경

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,17 +4,17 @@ import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import Style from '@/app/(home)/page.module.scss';
 import Button from '@/components/common/Button/Button';
-
-// 이미지 import
-import landingMainBannerLg from '@/assets/images/landing/lg_01.png';
-import landingMainBannerMd from '@/assets/images/landing/md_01.png';
-import landingMainBannerSm from '@/assets/images/landing/sm_01.png';
-import landingConImg01Lg from '@/assets/images/landing/lg_02.png';
-import landingConImg01Sm from '@/assets/images/landing/sm_02.png';
-import landingConImg02Lg from '@/assets/images/landing/lg_03.png';
-import landingConImg02Sm from '@/assets/images/landing/sm_03.png';
-import landingConImg03Lg from '@/assets/images/landing/lg_04.png';
-import landingConImg03Sm from '@/assets/images/landing/sm_04.png';
+import {
+  landingMainBannerLg,
+  landingMainBannerMd,
+  landingMainBannerSm,
+  landingConImg01Lg,
+  landingConImg01Sm,
+  landingConImg02Lg,
+  landingConImg02Sm,
+  landingConImg03Lg,
+  landingConImg03Sm,
+} from '@/assets/images/landing/index';
 
 export default function Home() {
   const [screenSize, setScreenSize] = useState<'desktop' | 'tablet' | 'mobile'>('desktop');
