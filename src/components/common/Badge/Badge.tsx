f36bb155276d28ab31df4a3fb28e48760b36ead3
Dang <wheodnjs2@naver.com>
2025-11-28 22:42:42 +0900

    fix: Badge 컴포넌트 설명 수정

--- src/components/common/Badge/Badge.tsx
+++ src/components/common/Badge/Badge.tsx
@@ -8,8 +8,8 @@
  * 아이콘 필요시 prefix에 원하는 아이콘을 넣는다. (텍스트)
  * <Badge prefix="₩">89,900</Badge>
  *
- * svg는 컴포넌트 형식으로 넣는다.
- * <Badge prefix={<Star />} className="bg-rating">4.8</Badge>
+ * 이미지를 직접 넣기도 가능합니다.
+ * <Badge prefix={<Image src={StarActive} width={20} alt="별점" />} className="bg-rating">4.8</Badge>
  *
  * 일반적인 뱃지 케이스
  * <Badge>일반</Badge>
@@ -19,9 +19,9 @@ import { ReactNode } from 'react';
 import styles from './Badge.module.scss';
 
 interface BadgeProps {
-  prefix?: ReactNode; // 앞에 들어갈 아이콘/문자
-  children: ReactNode; // 메인 텍스트
-  className?: string; // 필요 시 추가 스타일
+  prefix?: ReactNode;
+  children: ReactNode;
+  className?: string;
 }
 
 export default function Badge({ prefix, children, className }: BadgeProps) {
