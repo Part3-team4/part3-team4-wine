f179ad2bdca878dab2bd4c65b6461c2f52da207c
9tnqls6 <9tnqls6@gmail.com>
2025-11-28 16:14:43 +0900

    fix:page파일 초기화

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,20 +1,7 @@
-import SelectBox2 from '@/components/common/SelectBox2/SelectBox2';
-
 export const metadata = {
   title: '회원가입',
 };
 
 export default function Page() {
-  return (
-    <>
-      {' '}
-      <SelectBox2
-        options={[
-          { label: '체리', value: 'CHERRY' },
-          { label: '베리', value: 'BERRY' },
-          { label: '오크', value: 'OAK' },
-        ]}
-      />
-    </>
-  );
+  return <>회원가입</>;
 }
