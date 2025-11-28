2b0571dc89ab1657e4a10a2370d488caea508e97
9tnqls6 <9tnqls6@gmail.com>
2025-11-28 15:54:40 +0900

    refactor:기존 셀렉박스 삭제

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,7 +1,20 @@
+import SelectBox2 from '@/components/common/SelectBox2/SelectBox2';
+
 export const metadata = {
   title: '회원가입',
 };
 
 export default function Page() {
-  return <>회원가입</>;
+  return (
+    <>
+      {' '}
+      <SelectBox2
+        options={[
+          { label: '체리', value: 'CHERRY' },
+          { label: '베리', value: 'BERRY' },
+          { label: '오크', value: 'OAK' },
+        ]}
+      />
+    </>
+  );
 }
