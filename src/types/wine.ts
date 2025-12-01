f3148e7851f7b14dc555fb7e823af9b51cd853a6
k0nghaa <k0nghaalee@gmail.com>
2025-12-01 18:06:55 +0900

    refactor: 와인 타입과 리뷰 타입을 각각 별도 파일로 분리

---
+++ src/types/wine.ts
@@ -0,0 +1,14 @@
+export type Wine = {
+  id: string;
+  name: string;
+  region: string;
+  price: number;
+};
+
+export type Review = {
+  id: string;
+  name: string;
+  rating: number;
+  date: string;
+  content: string;
+};
