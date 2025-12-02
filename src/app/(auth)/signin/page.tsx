f0fdcf63d406c6975931c0e8181e2104116bdc54
9tnqls6 <9tnqls6@gmail.com>
2025-12-03 03:40:33 +0900

    style:로회 스타일 수정

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -71,16 +71,13 @@ export default function LoginPage() {
     const REST_API_KEY = process.env.NEXT_PUBLIC_KAKAO_REST_KEY;
     const REDIRECT_URI = 'http://localhost:3001/auth/kakao';
 
-    // ✅ 여기서 강제로 멈춤
-    alert(`KAKAO KEY: ${REST_API_KEY}`);
-
     const KAKAO_AUTH_URL =
       `https://kauth.kakao.com/oauth/authorize` +
       `?client_id=${REST_API_KEY}` +
       `&redirect_uri=${encodeURIComponent(REDIRECT_URI)}` +
       `&response_type=code`;
 
-    // window.location.href = KAKAO_AUTH_URL;
+    window.location.href = KAKAO_AUTH_URL;
   };
 
   // 비밀번호 분실
