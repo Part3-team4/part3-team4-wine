07a1af559d14292b87e267a3aa01e4c3915173ba
k0nghaa <k0nghaalee@gmail.com>
2025-12-02 16:14:51 +0900

    fix: 플러그인을 문자열 배열로 지정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,18 @@
-import { defineConfig, globalIgnores } from 'eslint/config';
-import prettierPlugin from 'eslint-plugin-prettier';
-import importPlugin from 'eslint-plugin-import';
+import { defineConfig } from 'eslint/config';
 
-export default defineConfig([
-  {
-    extends: ['next', 'plugin:prettier/recommended', 'eslint:recommended'],
-    plugins: {
-      prettier: prettierPlugin,
-      import: importPlugin,
-    },
-    rules: {
-      'prettier/prettier': 'error',
-
-      'import/order': [
-        'error',
-        {
-          groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index']],
-          'newlines-between': 'always',
-          alphabetize: {
-            order: 'asc',
-            caseInsensitive: true,
-          },
-        },
-      ],
-    },
+export default defineConfig({
+  extends: ['next', 'plugin:prettier/recommended', 'eslint:recommended'],
+  plugins: ['prettier', 'import'],
+  rules: {
+    'prettier/prettier': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index']],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
+    ],
   },
-  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
-]);
+  ignorePatterns: ['.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
+});
