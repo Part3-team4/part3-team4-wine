bb3744cc12e87a1c656a6b70ad9c4f0c73dffd0a
looks32 <wheodnjs2@naver.com>
2025-12-02 21:11:46 +0900

    fix: 리뷰 목록 필터 버튼 추가, 와인 등록 팝업 추가

--- src/app/(main)/wines/page.tsx
+++ src/app/(main)/wines/page.tsx
@@ -7,29 +7,97 @@ import { useEffect, useState } from 'react';
 import WineFilter from '@/components/features/WineFilter/WineFilter';
 import WineSlider from '@/components/features/WineSlider/WineSlider';
 import { api } from '@/libs/api';
+import Button from '@/components/common/Button/Button';
+import { useModal } from '@/hooks/useModal';
+import WineAddModal from '@/components/features/ModalFeatures/WineAddModal/WineAddModal';
 
 interface Wine {
   id: number;
   name: string;
-  rating: number;
+  avgRating: number;
   region: string;
   price: number;
   image?: string;
-  reviewLength: number;
-  reviewContent: string;
+  reviewCount: number;
+  recentReview?: {
+    content: string;
+  } | null;
 }
 
 export default function Page() {
+  // 와인 등록하기
+  const { open, close } = useModal();
+
   const [searchText, setSearchText] = useState('');
+
   const [wines, setWines] = useState<Wine[]>([]);
+  const [recommendedWines, setRecommendedWines] = useState([]);
+
+  // 와인 등록하기
+  async function createWine(data: {
+    name: string;
+    region: string;
+    image: string;
+    price: number;
+    type: 'RED' | 'WHITE' | 'SPARKLING';
+  }) {
+    const res = await api.post('/wines', data);
+    return res.data;
+  }
+
+  // 이미지 업로드
+  async function uploadImage(file: File) {
+    const formData = new FormData();
+    formData.append('image', file);
+
+    const res = await api.post('/images/upload', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+    return res.data;
+  }
+
+  const handleOpenWineAddModal = () => {
+    const modalId = open(
+      <WineAddModal
+        onSubmit={async (data) => {
+          // 1) 이미지 업로드 (있을 경우만)
+          const imageUrl = data.image ? (await uploadImage(data.image)).url : '';
+
+          // 2) 와인 등록 API 호출
+          await createWine({
+            name: data.name,
+            region: data.region,
+            price: data.price,
+            type: data.type,
+            image: imageUrl,
+          });
+
+          // 3) 등록 성공 후 모달 닫기
+          close(modalId);
+        }}
+      />,
+    );
+  };
 
   // wine불러오기
   useEffect(() => {
+    // 와인 리스트
     const fetchWines = async () => {
-      const res = await api.get('/wines?limit=20');
+      const res = await api.get('/wines?limit=50');
+      console.log(res.data.list);
       setWines(res.data.list);
     };
+
+    // 추천 와인 리스트
+    const fetchRecommendedWines = async () => {
+      const res = await api.get('/wines/recommended?limit=20');
+      setRecommendedWines(res.data);
+    };
+
     fetchWines();
+    fetchRecommendedWines();
   }, []);
 
   return (
@@ -37,7 +105,7 @@ export default function Page() {
       <div className={styles.slideArea}>
         <h2>이번 달 추천 와인</h2>
         <div className={styles.recommendSlide}>
-          <WineSlider />
+          {recommendedWines.length > 0 && <WineSlider data={recommendedWines} />}
         </div>
       </div>
       <div className={styles.contentArea}>
@@ -49,6 +117,19 @@ export default function Page() {
           />
           <div className={styles.wineFilterArea}>
             <WineFilter />
+            <div className={styles.buttonArea}>
+              <div className={styles.half}>
+                <Button size="medium" variant="tinted">
+                  필터 적용
+                </Button>
+                <Button size="medium" variant="outlined">
+                  초기화
+                </Button>
+              </div>
+              <Button size="medium" fullWidth onClick={handleOpenWineAddModal}>
+                와인 등록하기
+              </Button>
+            </div>
           </div>
         </div>
         <div className={styles.wineListArea}>
@@ -72,11 +153,11 @@ export default function Page() {
                 <ListWineCard
                   id={wine.id}
                   name={wine.name}
-                  rating={wine.rating}
+                  rating={wine.avgRating}
                   region={wine.region}
                   price={wine.price}
-                  reviewLength={wine.reviewLength}
-                  reviewContent={wine.reviewContent}
+                  reviewLength={wine.reviewCount}
+                  reviewContent={wine.recentReview?.content}
                   image={wine.image}
                 />
               </li>
