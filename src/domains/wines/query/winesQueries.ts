ce00842c6d198b2752a8ddb1f9d9913299b5eb78
hs <tlsgustn93@naver.com>
2025-12-02 20:45:38 +0900

    QueryOption 추가

--- src/domains/wines/query/winesQueries.ts
+++ src/domains/wines/query/winesQueries.ts
@@ -1,3 +1,4 @@
+import { queryOptions } from '@tanstack/react-query';
 import { winesApi } from '../api/winesApi';
 import { winesQueryKeys } from './winesQueryKeys';
 import type { WineListParams } from '../types/wine.types';
@@ -6,8 +7,10 @@ import type { WineListParams } from '../types/wine.types';
  * Wines Query Options Factory
  */
 export const winesQueries = {
-  list: (params: WineListParams) => ({
-    queryKey: winesQueryKeys.list(params),
-    queryFn: () => winesApi.getWines(params),
-  }),
+  list: (params: WineListParams) =>
+    queryOptions({
+      queryKey: winesQueryKeys.list(params),
+      queryFn: () => winesApi.getWines(params),
+      staleTime: 0,
+    }),
 };
